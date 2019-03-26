1d5e24cfcc2ab4ee3860f7179c1a058a3563c320
Giang Bui <giangbui0816@gmail.com>
2019-03-26 15:32:33 -0500

    feat(indexing): index list of uuid in a manifest

---
+++ main.py
@@ -0,0 +1,69 @@
+from indexclient.client import IndexClient
+from cdislogging import get_logger
+
+from settings import INDEXD
+from utils import get_fileinfos_from_tsv_manifest
+
+logger = get_logger(__name__)
+
+def manifest_indexing(manifest, prefix=None):
+    indexclient = IndexClient(
+        INDEXD["host"],
+        INDEXD["version"],
+        (INDEXD["auth"]["username"], INDEXD["auth"]["password"]),
+    )
+
+    files = get_fileinfos_from_tsv_manifest(manifest)
+    prefix = prefix or ""
+
+    for fi in files:
+        try:
+            doc = indexclient.get(prefix + "/" + fi.get("id"))
+            url = fi.get("url")
+            if doc is not None:
+                need_update = False
+                if url not in doc.urls:
+                    doc.urls.append(url)
+                    need_update = True
+
+                if fi.get("acl") in {"[u'open']", "['open']"}:
+                    acl = ["*"]
+                else:
+                    acl = fi.get("acl")[1:-1].split(",")
+
+                if doc.acl != acl:
+                    doc.acl = acl
+                    need_update = True
+
+                if need_update:
+                    doc.patch()
+            else:
+                doc = indexclient.create(
+                        did=prefix + "/" + fi.get("id"),
+                        hashes={"md5": fi.get("md5")},
+                        size=fi.get("size", 0),
+                        acl=acl,
+                        urls=[url],
+                    )
+
+        except Exception as e:
+            # Don't break for any reason
+            logger.error("Can not update/create an indexd record with uuid {}. Detail {}".format(fi.get("id"))
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title="action", dest="action")
+
+    indexing_cmd = subparsers.add_parser("indexing")
+    indexing_cmd.add_argument("--prefix", required=True, help="indexd prefix")
+    indexing_cmd.add_argument("--manifest", required=True, help="The manifest path")
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+
+    if args.action == "indexing":
+        submit_test_data(args.host, args.project, args.dir, args.access_token_file, int(args.chunk_size))
+        return
+    manifest_indexing
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import csv
 
 
-def get_fileinfos_from_tsv_manifest(manifest_file, start=None, end=None, dem="\t"):
+def get_fileinfos_from_tsv_manifest(manifest_file, dem="\t"):
     """
     get file info from tsv manifest
     """
@@ -12,8 +12,5 @@ def get_fileinfos_from_tsv_manifest(manifest_file, start=None, end=None, dem="\t
             row["size"] = int(row["size"])
             files.append(row)
 
-    start_idx = start if start else 0
-    end_idx = end if end else len(files)
-
-    return files[start_idx:end_idx]
+    return files
 
