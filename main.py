490a59bba64748fd4ef58c9337941cbd0a831a75
Alexander VanTol <avantol13@gmail.com>
2019-10-29 14:05:08 -0500

    feat(urls) update existing urls, allow list

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from utils import get_fileinfos_from_tsv_manifest
 
 logger = get_logger(__name__)
 
+
 def manifest_indexing(manifest, prefix=None):
     """
     Loop through all the files in the manifest, update/create records in indexd
@@ -29,20 +30,22 @@ def manifest_indexing(manifest, prefix=None):
     number_indexed_files = 0
     for fi in files:
         try:
-            url = fi.get("url")
+            urls = fi.get("url").split(" ")
 
             if fi.get("acl").lower() in {"[u'open']", "['open']"}:
                 acl = ["*"]
             else:
-                acl = [element.strip().replace("'", "") for element in fi.get("acl")[1:-1].split(",")]
+                acl = [
+                    element.strip().replace("'", "")
+                    for element in fi.get("acl")[1:-1].split(",")
+                ]
 
             doc = indexclient.get(prefix + fi.get("GUID"))
             if doc is not None:
                 need_update = False
-                if url not in doc.urls:
-                    doc.urls.append(url)
+                if set(urls) != set(doc.urls):
+                    doc.urls = urls
                     need_update = True
-
 
                 if set(doc.acl) != set(acl):
                     doc.acl = acl
@@ -52,19 +55,25 @@ def manifest_indexing(manifest, prefix=None):
                     doc.patch()
             else:
                 doc = indexclient.create(
-                        did=prefix + fi.get("GUID"),
-                        hashes={"md5": fi.get("md5")},
-                        size=fi.get("size", 0),
-                        acl=acl,
-                        urls=[url],
-                    )
-            number_indexed_files +=1
+                    did=prefix + fi.get("GUID"),
+                    hashes={"md5": fi.get("md5")},
+                    size=fi.get("size", 0),
+                    acl=acl,
+                    urls=urls,
+                )
+            number_indexed_files += 1
             if number_indexed_files % 10 == 0 or number_indexed_files == len(files):
-                logger.info("Progress {}%".format(number_indexed_files*100.0/len(files)))
+                logger.info(
+                    "Progress {}%".format(number_indexed_files * 100.0 / len(files))
+                )
 
         except Exception as e:
             # Don't break for any reason
-            logger.error("Can not update/create an indexd record with uuid {}. Detail {}".format(fi.get("GUID")))
+            logger.error(
+                "Can not update/create an indexd record with uuid {}. Detail {}".format(
+                    fi.get("GUID")
+                )
+            )
 
 
 def parse_arguments():
@@ -77,9 +86,8 @@ def parse_arguments():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_arguments()
 
     if args.action == "indexing":
         manifest_indexing(args.manifest, prefix=args.prefix)
-
